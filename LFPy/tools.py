40934c93ca127ead0b61a3011e4d4c5417a9a8b4
ehagen <ehagen@2d86cfe5-f61f-4435-902d-8fa86043e322>
2011-07-13 14:34:21 +0000

    added some DC filter

--- LFPy/tools.py
+++ LFPy/tools.py
@@ -3,6 +3,7 @@
 All rights reserved.'''
 import cPickle
 import pylab as pl
+import scipy.signal as ss
 
 def load(filename):
     '''generic loading of cPickled objects from file'''
@@ -11,15 +12,22 @@ def load(filename):
     filen.close()
     return obj
 
-def noise_brown(t, n=1, weight=1):
+def noise_brown(t, n=1, weight=1, cutDC=False):
     '''return 1/f^2 noise of shape(n, t.size) obtained by taking the
-    cumulative sum of gaussian white noise, with rms weight'''
+    cumulative sum of gaussian white noise, with rms weight. If cutDC=True,
+    the noise is high-pass filtered with a very low cutoff frequency'''
     noise = []
+    if cutDC:
+        fcut = 4./t.size
+        [b, a] = ss.butter(1, fcut, btype='high')
+        
     for i in xrange(n):
-        x = pl.normal(size=t.size).cumsum()
+        x = pl.normal(size=t.size+10000).cumsum()
+        if cutDC:
+            x = ss.lfilter(b, a, x)
         x /= pl.rms_flat(x)
         x *= weight
-        noise.append(x)
-    
+        noise.append(x[10000:])
+        
     return pl.array(noise)
     
