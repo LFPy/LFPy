b291b8a0a585c22f58f18b2bed23b0df8dc63148
ehagen <ehagen@2d86cfe5-f61f-4435-902d-8fa86043e322>
2011-12-12 15:04:56 +0000

    added missing verbose flag

--- LFPy/electrodethreaded.pyx
+++ LFPy/electrodethreaded.pyx
@@ -22,12 +22,13 @@ class ElectrodeThreaded(Electrode):
                  perCellLFP=False, method='linesource', 
                  color='g', marker='o',
                  from_file=False, cellfile=None,
-                 NUMBER_OF_PROCESSES=None):
+                 NUMBER_OF_PROCESSES=None, verbose=False):
         '''Initialization of class ElectrodeThreaded, with electrode setup
         inherited from class ElectrodeSetup'''
         Electrode.__init__(self, cell, sigma, x, y, z,
                                 N, r, n, r_z, perCellLFP,
-                                method, color, marker, from_file, cellfile)
+                                method, color, marker, from_file,
+                                cellfile, verbose)
         #set the numbers of processes to use, if none use all available cores
         if NUMBER_OF_PROCESSES == None:
             NUMBER_OF_PROCESSES = cpu_count()
