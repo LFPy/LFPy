c5b6f1c1e2e0a1b17816e077f6a5504b7cea3451
szleski <szleski@2d86cfe5-f61f-4435-902d-8fa86043e322>
2012-02-01 13:35:28 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup 
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
 
 setup(
     name = "LFPy",
-    version = "0.1",
-    package_dir={'trunk': 'LFPy'},
-    packages = ['LFPy'],
+    version = "1.0", 
     author = "The LFPy Community",
-    description = "A simulation environment for LFP built on NEURON",
+    packages = ['LFPy'], 
+    cmdclass = { 'build_ext' : build_ext}, 
+    ext_modules = [
+        Extension('LFPy.lfpcalc', 
+        ['LFPy/lfpcalc.pyx'],
+        include_dirs=[numpy.get_include()]),
+        Extension('LFPy.run_simulation', 
+        ['LFPy/run_simulation.pyx'],
+        include_dirs=[numpy.get_include()]),
+        Extension('LFPy.recextelectrodethreaded', 
+        ['LFPy/recextelectrodethreaded.pyx'],
+        include_dirs=[numpy.get_include()])
+        ],
+    url='http://pypi.python.org/pypi/LFPy/',
+    license='LICENSE.txt',
+    description='A simulation environment for LFP built on NEURON',
+    long_description=open('README.txt').read()
     )
