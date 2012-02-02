2cc5a7ad197ef50c2d453f6af4e004f1e27813aa
henrli <henrli@2d86cfe5-f61f-4435-902d-8fa86043e322>
2012-02-02 15:41:29 +0000

    Changes in example1 ./HL

--- examples/example1.py
+++ examples/example1.py
@@ -31,26 +31,26 @@ cell = LFPy.Cell(**cell_parameters)
 # Define synapse parameters
 
 synapse_parameters = {
-    'idx' : cell.get_closest_idx(x=0., y=0., z=800.),
+    'idx' : cell.get_closest_idx(x=-50., y=0., z=900.),
     'e' : 0.,                                # reversal potential
     'syntype' : 'ExpSyn',                   # synapse type
     'tau' : 2.,                              # syn. time constant
     'weight' : .1,                       # syn. weight
-    'record_current' : True                 # syn. current record
+    'record_current':True,
 }
 
 # Create synapse and input spike time
 
 synapse = LFPy.Synapse(cell,**synapse_parameters)
-synapse.set_spike_times(cell, np.array([50.]))
+synapse.set_spike_times(np.array([50.]))
 
 # Define electrode parameters
 
 # But first create a grid of measurement locations
 
-x_elec = np.tile(np.arange(-100,100,10),10)
+x_elec = np.tile(np.arange(-400,401,40),10)
 y_elec = np.zeros(len(x_elec))
-z_elec = np.repeat(np.arange(-100,100,10),10)
+z_elec = np.repeat(np.arange(-400,1201,1600./20),10)
 
 electrode_parameters = {
     'sigma' : 0.3,   # extracellular conductivity
@@ -70,11 +70,33 @@ electrode.calc_lfp()
 
 # Find max(abs(LFP))
 
-# max_abs_LFP = np.max(np.abs(electrode.LFP))
+max_abs_LFP = np.max(np.abs(electrode.LFP),1)
+sign_max_abs_LFP = np.zeros(len(max_abs_LFP))
+for i in xrange(len(sign_max_abs_LFP)):
+    sign_max_abs_LFP[i] = np.sign(electrode.LFP[i]\
+        [np.where(np.abs(electrode.LFP[i])==max_abs_LFP[i])])
 
 # Plot results
-# 
-# plt.figure()
-# n_contours = 10
-# plt.contour(x_elec,z_elec,max_abs_LFP,n_contours)
-# 
+def plot_morphology():
+    # Plot dendrites
+    for i in xrange(cell.xend.size):     
+        plt.plot([cell.xstart[i],cell.xend[i]],\
+            [cell.zstart[i],cell.zend[i]],color='k')
+    # Plot synapses
+    for i in xrange(len(cell.synapses)):
+        plt.plot([cell.synapses[i].x],[cell.synapses[i].z],\
+            color=cell.synapses[i].color,marker=cell.synapses[i].marker,\
+            markersize=10)
+
+plt.figure()
+
+plot_morphology()
+n_contours = 20
+X = np.reshape(x_elec,(10,21))
+Y = np.reshape(z_elec,(10,21))
+Z = np.reshape(sign_max_abs_LFP*max_abs_LFP,(10,21))
+plt.contourf(X,Y,Z,n_contours)
+
+
+# Figure formatting
+plt.axis('equal')
