7b17f8187b37557640375f07c5ec63fe7e771aab
henrli <henrli@2d86cfe5-f61f-4435-902d-8fa86043e322>
2012-02-03 14:10:19 +0000

    Updated example 1./HL

--- examples/example1.py
+++ examples/example1.py
@@ -2,13 +2,12 @@ import LFPy
 import numpy as np
 import matplotlib.pylab as plt
 
-plt.ion()
+# Example plot for LFPy: Single-synapse contribution to the LFP
 
 # Define parameters
 
 cell_parameters = {          #various cell parameters,
     'morphology' : 'morphologies/L5_Mainen96_LFPy.hoc', #Mainen&Sejnowski, Nature, 1996
-#     'morphology' : 'morphologies/CablesIncluded.xml',
     'rm' : 30000.,       #membrane resistance
     'cm' : 1.0,        #membrane capacitance
     'Ra' : 150.,         #axial resistance
@@ -31,7 +30,7 @@ cell = LFPy.Cell(**cell_parameters)
 # Define synapse parameters
 
 synapse_parameters = {
-    'idx' : cell.get_closest_idx(x=0., y=0., z=200.),
+    'idx' : cell.get_closest_idx(x=0., y=0., z=900.),#200.
     'e' : 0.,                                # reversal potential
     'syntype' : 'ExpSyn',                   # synapse type
     'tau' : 10.,                              # syn. time constant
@@ -48,8 +47,11 @@ synapse.set_spike_times(np.array([50.]))
 
 # But first create a grid of measurement locations
 
-x = np.arange(-375, 376, 50)
-z = np.arange(-275, 1201, 50)
+# x = np.arange(-375, 376, 50)
+# z = np.arange(-275, 1201, 50)
+x = np.arange(-500, 501, 50)
+z = np.arange(-400, 1201, 50)
+
 X, Z = np.meshgrid(x, z)
 Y = np.zeros(np.shape(X))
 
@@ -60,51 +62,84 @@ electrode_parameters = {
     'z' : Z.flatten()
 }
 
+# Create electrode
+
+electrode = LFPy.RecExtElectrode(**electrode_parameters)
+
 # Run simulation
 
-cell.simulate(rec_imem=True, rec_isyn=True, rec_vmem=True)
+print "running simulation..."
+cell.simulate(electrode=electrode,rec_isyn=True)
+print "done"
 
-# Set up electrode and calculate extracellular potential
+# Plotting #####################################################################
 
-electrode = LFPy.RecExtElectrode(cell,**electrode_parameters)
-electrode.calc_lfp()
+# functino for plotting the morphology of the cell 
 
-# Find max(abs(LFP))
-
-max_abs_LFP = np.max(np.abs(electrode.LFP),1)
-sign_max_abs_LFP = np.zeros(len(max_abs_LFP))
-for i in xrange(len(sign_max_abs_LFP)):
-    sign_max_abs_LFP[i] = np.sign(electrode.LFP[i]\
-        [np.where(np.abs(electrode.LFP[i])==max_abs_LFP[i])])
-
-# Plot results
-def plot_morphology():
-    # Plot dendrites
+def plot_morphology(plot_synapses=False,color='k'):
     for i in xrange(cell.xend.size):     
+        if len(color)>1:
+            col=str(color[i])
+        else:
+            col = color
         plt.plot([cell.xstart[i],cell.xend[i]],\
-            [cell.zstart[i],cell.zend[i]],color='k')
-    # Plot synapses
-    for i in xrange(len(cell.synapses)):
-        plt.plot([cell.synapses[i].x],[cell.synapses[i].z],\
-            color=cell.synapses[i].color,marker=cell.synapses[i].marker,\
-            markersize=10)
+            [cell.zstart[i],cell.zend[i]],color=col)
+    if plot_synapses:
+        for i in xrange(len(cell.synapses)):
+            plt.plot([cell.synapses[i].x],[cell.synapses[i].z],\
+                color=cell.synapses[i].color,marker=cell.synapses[i].marker,\
+                markersize=10)
+
+# Create figure
+
+plt.ion() # Turn on interactive plotting in matplotlib (optional)
 
 plt.figure()
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.4, hspace=0.4)
 
-plot_morphology()
-n_contours = 100
+# Plot LFP around the cell with in color and with equipotential lines
 
-LFP = np.reshape(sign_max_abs_LFP*max_abs_LFP,(len(z),len(x)))
+plt.subplot(121,aspect='equal')
+
+t_show = 60 #time point to show LFP
+tidx = np.where(cell.tvec==t_show)
+
+plot_morphology(plot_synapses=True)
+n_contours = 200
+n_contours_black = 40
+
+LFP = np.reshape(np.arcsinh(electrode.LFP[:,tidx]),(len(z),len(x)))
 plt.contourf(X,Z,LFP,n_contours)
+plt.clim((-0.00007,.00002))
+plt.contour(X,Z,LFP,n_contours_black,colors='k')
 
-# LFP = np.reshape(np.log(max_abs_LFP),(len(z),len(x)))
-# plt.contourf(X,Z,LFP,n_contours)
+# Plot synaptic input current
 
-plt.figure()
-plt.plot(cell.tvec,cell.somav)
-
-plt.figure()
+plt.subplot(222)
 plt.plot(cell.tvec,cell.synapses[0].i)
 
+# Plot soma potential
+
+plt.subplot(224)
+plt.plot(cell.tvec,cell.somav)
+
 # Figure formatting
-plt.axis('equal')
+
+plt.suptitle('example 1',fontsize=14)
+
+plt.subplot(121,aspect='equal')
+plt.axis('off')
+plt.title('LFP at t='+str(t_show)+' ms',fontsize=12)
+
+plt.subplot(222)
+plt.title('synaptic input current',fontsize=12)
+plt.ylabel('(nA)')
+plt.xlabel('time (ms)')
+
+plt.subplot(224)
+plt.title('somatic membrane potential',fontsize=12)
+plt.ylabel('(mV)')
+plt.xlabel('time (ms)')
+
+# Optionally save figure (uncomment the line below)
+# plt.savefig('example1.pdf')
