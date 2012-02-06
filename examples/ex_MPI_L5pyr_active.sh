282ef38d28ba3aabe357289ebe94f02de6bfd081
ehagen <ehagen@2d86cfe5-f61f-4435-902d-8fa86043e322>
2012-02-06 10:55:00 +0000

    removed MPI-examplesvn rm example5_population_MPI.py svn rm example5_population_MPI.py svn rm example5_population_MPI.py svn rm example5_population_MPI.py

--- examples/ex_MPI_L5pyr_active.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#PBS -lnodes=1:ppn=8
-#PBS -lwalltime=0:10:00
-#PBS -A nn4661k
-
-cd $PBS_O_WORKDIR
-export LD_PRELOAD=libmkl_intel_lp64.so:libmkl_intel_thread.so:libmkl_core.so:libguide.so
-mpirun -n 8 -x LD_PRELOAD python ex_MPI_L5pyr_active.py
-wait
