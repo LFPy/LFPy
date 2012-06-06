08cf13f77009d2689f7b8e92ccfb6a4057c8f2b2
ehagen <espenhgn@users.noreply.github.com>
2012-06-06 09:07:21 +0000

    using buffered driver='stdio' for storing LFPs during sim

--- LFPy/run_simulation.py
+++ LFPy/run_simulation.py
@@ -180,7 +180,7 @@ def _run_simulation_with_electrode(cell, electrode,
         #ensure right ending:
         if file_name.split('.')[-1] != 'h5':
             file_name += '.h5'
-        el_LFP_file = h5py.File(file_name, 'w')
+        el_LFP_file = h5py.File(file_name, 'w', driver='stdio')
         i = 0
         for coeffs in electrodecoeffs:
             el_LFP_file['electrode%.3i' % i] = np.empty((coeffs.shape[0],
