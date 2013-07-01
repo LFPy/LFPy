2444f9092db61799008cd3ce0c9922337bb84cb0
ehagen <ehagen@2d86cfe5-f61f-4435-902d-8fa86043e322>
2013-07-01 14:36:59 +0000

    renamed example files

--- trunk/examples/example4.py
+++ trunk/examples/example4.py
@@ -1,177 +1,74 @@
 #!/usr/bin/env python
 '''
-################################################################################
-#
-# This is an example scripts using LFPy with a passive cell model adapted from
-# Mainen and Sejnowski, Nature 1996, for the original files, see
-# http://senselab.med.yale.edu/modeldb/ShowModel.asp?model=2488
-#
-# Here, excitatory and inhibitory neurons are distributed on different parts of
-# the morphology, with stochastic spike times produced by the
-# LFPy.inputgenerators.stationary_gamma() function.
-#
-# Same as "example3.py", just without the active conductances
-################################################################################
+Example plot for LFPy: Single-synapse contribution to the LFP
 '''
-
-# importing some modules, setting some matplotlib values for pl.plot.
 import LFPy
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size' : 12,
-    'figure.facecolor' : '1',
-    'wspace' : 0.5, 'hspace' : 0.5})
-
-#seed for random generation
-np.random.seed(1234)
 
 ################################################################################
-# A couple of function declarations
+# Main script, set parameters and create cell, synapse and electrode objects
 ################################################################################
 
-def insert_synapses(synparams, section, n, spTimesFun, args):
-    '''find n compartments to insert synapses onto'''
-    idx = cell.get_rand_idx_area_norm(section=section, nidx=n)
-
-    #Insert synapses in an iterative fashion
-    for i in idx:
-        synparams.update({'idx' : int(i)})
-
-        # Some input spike train using the function call
-        spiketimes = spTimesFun(args[0], args[1], args[2], args[3], args[4])
-        
-        # Create synapse(s) and setting times using the Synapse class in LFPy
-        s = LFPy.Synapse(cell, **synparams)
-        s.set_spike_times(spiketimes)
-
-################################################################################
-# Define parameters, using dictionaries
-# It is possible to set a few more parameters for each class or functions, but
-# we chose to show only the most important ones here.
-################################################################################
-
-#define cell parameters used as input to cell-class
-cellParameters = {
-    'morphology' : 'morphologies/L5_Mainen96_wAxon_LFPy.hoc',
-    'rm' : 30000,               # membrane resistance
-    'cm' : 1.0,                 # membrane capacitance
-    'Ra' : 150,                 # axial resistance
-    'v_init' : -65,             # initial crossmembrane potential
-    'e_pas' : -65,              # reversal potential passive mechs
-    'passive' : True,           # switch on passive mechs
-    'nsegs_method' : 'lambda_f',# method for setting number of segments,
-    'lambda_f' : 100,           # segments are isopotential at this frequency
-    'timeres_NEURON' : 2**-4,   # dt of LFP and NEURON simulation.
-    'timeres_python' : 2**-4,
-    'tstartms' : -100,          #start time, recorders start at t=0
-    'tstopms' : 200,            #stop time of simulation
-    #'custom_code'  : ['active_declarations_example3.hoc'], # will run this file
+# Define cell parameters
+cell_parameters = {          # various cell parameters,
+    'morphology' : 'morphologies/L5_Mainen96_LFPy.hoc', # Mainen&Sejnowski, 1996
+    'rm' : 30000.,      # membrane resistance
+    'cm' : 1.0,         # membrane capacitance
+    'Ra' : 150.,        # axial resistance
+    'v_init' : -65.,    # initial crossmembrane potential
+    'e_pas' : -65.,     # reversal potential passive mechs
+    'passive' : True,   # switch on passive mechs
+    'nsegs_method' : 'lambda_f',
+    'lambda_f' : 100.,
+    'timeres_NEURON' : 2.**-3,   # [ms] dt's should be in powers of 2 for both,
+    'timeres_python' : 2.**-3,   # need binary representation
+    'tstartms' : -100., # start time of simulation, recorders start at t=0
+    'tstopms' : 100.,   # stop simulation at 200 ms. These can be overridden
+                        # by setting these arguments in cell.simulation()
 }
 
-# Synaptic parameters taken from Hendrickson et al 2011
-# Excitatory synapse parameters:
-synapseParameters_AMPA = {
-    'e' : 0,                    #reversal potential
-    'syntype' : 'Exp2Syn',      #conductance based exponential synapse
-    'tau1' : 1.,                #Time constant, rise
-    'tau2' : 3.,                #Time constant, decay
-    'weight' : 0.005,           #Synaptic weight
-    'color' : 'r',              #for plt.plot
-    'marker' : '.',             #for plt.plot
-    'record_current' : True,    #record synaptic currents
-}
-# Excitatory synapse parameters
-synapseParameters_NMDA = {         
-    'e' : 0,
-    'syntype' : 'Exp2Syn',
-    'tau1' : 10.,
-    'tau2' : 30.,
-    'weight' : 0.005,
-    'color' : 'm',
-    'marker' : '.',
+# Create cell
+cell = LFPy.Cell(**cell_parameters)
+
+# Define synapse parameters
+synapse_parameters = {
+    'idx' : cell.get_closest_idx(x=0., y=0., z=900.),
+    'e' : 0.,                   # reversal potential
+    'syntype' : 'ExpSyn',       # synapse type
+    'tau' : 10.,                # syn. time constant
+    'weight' : .001,            # syn. weight
     'record_current' : True,
 }
-# Inhibitory synapse parameters
-synapseParameters_GABA_A = {         
-    'e' : -80,
-    'syntype' : 'Exp2Syn',
-    'tau1' : 1.,
-    'tau2' : 12.,
-    'weight' : 0.005,
-    'color' : 'b',
-    'marker' : '.',
-    'record_current' : True
-}
-# where to insert, how many, and which input statistics
-insert_synapses_AMPA_args = {
-    'section' : 'apic',
-    'n' : 100,
-    'spTimesFun' : LFPy.inputgenerators.stationary_gamma,
-    'args' : [cellParameters['tstartms'], cellParameters['tstopms'], 0.5, 40,
-              cellParameters['tstartms']]
-}
-insert_synapses_NMDA_args = {
-    'section' : ['dend', 'apic'],
-    'n' : 15,
-    'spTimesFun' : LFPy.inputgenerators.stationary_gamma,
-    'args' : [cellParameters['tstartms'], cellParameters['tstopms'], 2, 50,
-              cellParameters['tstartms']]
-}
-insert_synapses_GABA_A_args = {
-    'section' : 'dend',
-    'n' : 100,
-    'spTimesFun' : LFPy.inputgenerators.stationary_gamma,
-    'args' : [cellParameters['tstartms'], cellParameters['tstopms'], 0.5, 40,
-              cellParameters['tstartms']]
+
+# Create synapse and set time of synaptic input
+synapse = LFPy.Synapse(cell, **synapse_parameters)
+synapse.set_spike_times(np.array([20.]))
+
+
+# Create a grid of measurement locations, in (mum)
+X, Z = np.mgrid[-500:501:20, -400:1201:40]
+Y = np.zeros(X.shape)
+
+# Define electrode parameters
+electrode_parameters = {
+    'sigma' : 0.3,      # extracellular conductivity
+    'x' : X.flatten(),  # electrode requires 1d vector of positions
+    'y' : Y.flatten(),
+    'z' : Z.flatten()
 }
 
-# Define electrode geometry corresponding to a laminar electrode, where contact
-# points have a radius r, surface normal vectors N, and LFP calculated as the
-# average LFP in n random points on each contact:
-N = np.empty((16, 3))
-for i in range(N.shape[0]): N[i,] = [1, 0, 0] #normal unit vec. to contacts
-# put parameters in dictionary
-electrodeParameters = {
-    'sigma' : 0.3,              # Extracellular potential
-    'x' : np.zeros(16) + 25,      # x,y,z-coordinates of electrode contacts
-    'y' : np.zeros(16),
-    'z' : np.linspace(-500, 1000, 16),
-    'n' : 20,
-    'r' : 10,
-    'N' : N,
-}
+# Create electrode object
+electrode = LFPy.RecExtElectrode(**electrode_parameters)
 
-# Parameters for the cell.simulate() call, recording membrane- and syn.-currents
-simulationParameters = {
-    'rec_imem' : True,  # Record Membrane currents during simulation
-    'rec_isyn' : True,  # Record synaptic currents
-}
+# Run simulation, electrode object argument in cell.simulate
+print("running simulation...")
+cell.simulate(electrode=electrode, rec_isyn=True)
+print("done")
 
-################################################################################
-# Main simulation procedure
-################################################################################
-
-#Initialize cell instance, using the LFPy.Cell class
-cell = LFPy.Cell(**cellParameters)
-
-#Insert synapses using the function defined earlier
-insert_synapses(synapseParameters_AMPA, **insert_synapses_AMPA_args)
-insert_synapses(synapseParameters_NMDA, **insert_synapses_NMDA_args)
-insert_synapses(synapseParameters_GABA_A, **insert_synapses_GABA_A_args)
-
-#perform NEURON simulation, results saved as attributes in the cell instance
-cell.simulate(**simulationParameters)
-
-# Initialize electrode geometry, then calculate the LFP, using the
-# LFPy.RecExtElectrode class. Note that now cell is given as input to electrode
-# and created after the NEURON simulations are finished
-electrode = LFPy.RecExtElectrode(cell, **electrodeParameters)
-print('simulating LFPs....')
-electrode.calc_lfp()
-print('done')
-
-#plotting some variables and geometry, saving output to .pdf.
-from example_suppl import plot_ex3
-fig = plot_ex3(cell, electrode)
-#fig.savefig('example4.pdf')
+#create a plot
+from example_suppl import plot_ex1
+fig = plot_ex1(cell, electrode, X, Y, Z)
+# Optionally save figure (uncomment the line below)
+# fig.savefig('example1.pdf')
 plt.show()
