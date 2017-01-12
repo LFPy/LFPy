a3905a019ac79f39eb75f12fbd1b90d9c2147ba1
Espen Hagen <e.hagen@fz-juelich.de>
2017-01-12 15:43:37 +0100

    write verbose NEURON install info to file

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-pip install scipy cython coveralls #numpy nose
+#pip install scipy cython coveralls #numpy nose
 # install NEURON
 export PWD=pwd
 cd $HOME
 wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
 tar -xf nrn-7.4.tar.gz
 cd nrn-7.4
-sh build.sh
-./configure --prefix=$HOME/.local/nrn --without-iv --with-nrnpython
-make -j4
-make install
+echo "installing NEURON:"
+echo "running sh build.sh"
+sh build.sh > build.outp
+echo "running ./configure"
+./configure --prefix=$HOME/.local/nrn --without-iv --with-nrnpython > configure.outp
+echo "running make"
+make -j4 > make.outp
+echo "running make install"
+make install > make_install.outp
 cd src/nrnpython
+echo "installing neuron python module"
 python setup.py install --user
 cd $PWD
 export PATH=$HOME/.local/bin:$HOME/.local/nrn/x86_64/bin:$PATH
--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -9,8 +9,8 @@
 set -e
 
 python --version
-#python -c "import numpy; print('numpy %s' % numpy.__version__)"
-#python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
 if [[ "$COVERAGE" == "true" ]]; then
     nosetests --with-coverage --cover-package=LFPy
