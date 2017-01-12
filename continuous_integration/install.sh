7807a955e046f5fc7e600609876affc9b71f8b62
Espen Hagen <e.hagen@fz-juelich.de>
2017-01-12 16:05:34 +0100

    attempt to cache NEURON install folder

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-#pip install scipy cython coveralls #numpy nose
-# install NEURON
-export PWD=pwd
-cd $HOME
-wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
-tar -xf nrn-7.4.tar.gz
-cd nrn-7.4
-echo "installing NEURON:"
-echo "running sh build.sh"
-sh build.sh > build.outp
-echo "running ./configure"
-./configure --prefix=$HOME/.local/nrn --without-iv --with-nrnpython > configure.outp
-echo "running make"
-make -j4 > make.outp
-echo "running make install"
-make install > make_install.outp
-cd src/nrnpython
-echo "installing neuron python module"
-python setup.py install --user
-cd $PWD
-export PATH=$HOME/.local/bin:$HOME/.local/nrn/x86_64/bin:$PATH
-#export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.local/nrn/x86_64/bin:$PATH
 
+if hash nrniv 2>/dev/null; then
+    echo "NEURON appear to be installed"
+else
+    export PWD=pwd
+    cd $HOME
+    wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
+    tar -xf nrn-7.4.tar.gz
+    cd nrn-7.4
+    echo "installing NEURON:"
+    echo "running sh build.sh"
+    sh build.sh > build.outp
+    echo "running ./configure"
+    ./configure --prefix=$HOME/.local/nrn --without-iv --with-nrnpython > configure.outp
+    echo "running make"
+    make -j4 > make.outp
+    echo "running make install"
+    make install > make_install.outp
+    cd src/nrnpython
+    echo "installing neuron python module"
+    python setup.py install --user
+    cd $PWD
+    export PATH=$HOME/.local/nrn/x86_64/bin:$PATH
+fi
 
--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -12,6 +12,9 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
+# build LFPy inplace (compile cython extensions, NMODL files)
+python setup.py build_ext -i
+
 if [[ "$COVERAGE" == "true" ]]; then
     nosetests --with-coverage --cover-package=LFPy
 else
