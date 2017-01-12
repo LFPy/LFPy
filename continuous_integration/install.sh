de2e54dfb61714377160f16f1fcfb3678dcf1b32
Espen Hagen <e.hagen@fz-juelich.de>
2017-01-12 18:18:10 +0100

    test

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 export PATH=$HOME/.local/nrn/x86_64/bin:$PATH
 
-#if hash nrniv 2>/dev/null; then
-#    echo "NEURON appear to be installed"
-#else
-PWD=`pwd`
 cd $HOME
 wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
 tar -xf nrn-7.4.tar.gz
@@ -21,6 +17,5 @@ make install > /dev/null
 cd src/nrnpython
 echo "installing neuron python module"
 python setup.py install --user > /def/null
-cd $PWD
-#fi
 
+cd $TRAVIS_BUILD_DIR
