3013dc80175a1fb0a6cf3ebf4088525166799b3f
Espen Hagen <e.hagen@fz-juelich.de>
2017-04-07 18:50:16 +0200

    added copyright statement

--- examples/example_network/example_NetworkPopulation.py
+++ examples/example_network/example_NetworkPopulation.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+"""Demonstrate usage of LFPy.NetworkPopulation with a ball-and-stick type
+morphology with active HH channels inserted in the soma and passive-leak
+channels distributed throughout the apical stick. The corresponding morphology
+and template-specification is in the files BallAndStick.hoc and
+BallAndStickTemplate.hoc.
+
+Copyright (C) 2017 Computational Neuroscience Group, NMBU.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+"""
+
 from mpi4py.MPI import COMM_WORLD as COMM
 from LFPy import NetworkPopulation, NetworkCell
 # class NetworkCell parameters
