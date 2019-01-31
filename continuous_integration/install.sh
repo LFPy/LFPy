8fb085e7d4f733c678d88c6ae031e0f413eedfca
Espen Hagen <espehage@fys.uio.no>
2019-01-31 15:20:19 +0100

    disable cache, delete also neuron binary directory

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 export PATH=$HOME/.local/nrn/x86_64/bin:$PATH
 
-# make sure no neuron module installation exist
+# make sure no NEURON installation exist
 rm -r /home/travis/virtualenv/python*/lib/python*/site-packages/neuron
 rm /home/travis/virtualenv/python*/lib/python*/site-packages/NEURON*
+rm -r $HOME/.local/nrn
 
 cd $HOME
-wget https://github.com/neuronsimulator/nrn/archive/7.7.0.tar.gz
-tar -xf 7.7.0.tar.gz
-cd nrn-7.7.0
+git clone https://github.com/neuronsimulator/nrn.git
+cd nrn
+git checkout 7.7.0
+#wget https://github.com/neuronsimulator/nrn/archive/7.7.0.tar.gz
+#tar -xf 7.7.0.tar.gz
+#cd nrn-7.7.0
 echo "installing NEURON:"
 echo "running sh build.sh"
-sh build.sh #> /dev/null
+sh build.sh > /dev/null
 echo "running ./configure"
 ./configure --prefix=$HOME/.local/nrn --without-iv --with-nrnpython=python --with-mpi --disable-rx3d > /dev/null
 echo "running make"
