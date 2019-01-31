f824b8e8be3cc977b6771233153d0e14a1260a16
Espen Hagen <espehage@fys.uio.no>
2019-01-31 15:33:50 +0100

    now uses NEURON 7.7.0

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 export PATH=$HOME/.local/nrn/x86_64/bin:$PATH
 
-# make sure no NEURON installation exist
-rm -r /home/travis/virtualenv/python*/lib/python*/site-packages/neuron
-rm /home/travis/virtualenv/python*/lib/python*/site-packages/NEURON*
-rm -r $HOME/.local/nrn
-
 cd $HOME
 git clone https://github.com/neuronsimulator/nrn.git
 cd nrn
 git checkout 7.7.0
-#wget https://github.com/neuronsimulator/nrn/archive/7.7.0.tar.gz
-#tar -xf 7.7.0.tar.gz
-#cd nrn-7.7.0
 echo "installing NEURON:"
 echo "running sh build.sh"
 sh build.sh > /dev/null
@@ -24,5 +16,5 @@ echo "running make install"
 make install > /dev/null
 cd src/nrnpython
 echo "installing neuron python module"
-python setup.py install # > /dev/null
+python setup.py install > /dev/null
 cd $TRAVIS_BUILD_DIR
