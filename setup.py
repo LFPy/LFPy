847d472670697622128a8eaacb92dcd8c9f62ea4
Espen Hagen <espehage@fys.uio.no>
2019-07-10 16:24:39 +0200

    formatting, add docs to extras_require

--- setup.py
+++ setup.py
@@ -14,25 +14,24 @@ except ImportError as ie:
 try:
     import numpy
     from Cython.Distutils import build_ext
-    cmdclass = { 'build_ext' : build_ext}
-    ext_modules = [
-        Extension('LFPy.run_simulation',
-        [os.path.join('LFPy', 'run_simulation.pyx')],
-        include_dirs=[numpy.get_include()]),
-        Extension('LFPy.alias_method',
-        [os.path.join('LFPy', 'alias_method.pyx')],
-        include_dirs=[numpy.get_include()]),
-        ]
-except ImportError as ie:
+    cmdclass = {'build_ext': build_ext}
+    ext_modules = [Extension('LFPy.run_simulation',
+                   [os.path.join('LFPy', 'run_simulation.pyx')],
+                   include_dirs=[numpy.get_include()]),
+                   Extension('LFPy.alias_method',
+                   [os.path.join('LFPy', 'alias_method.pyx')],
+                   include_dirs=[numpy.get_include()]),
+                    ]
+except ImportError:
     print("'from Cython.Distutils import build_ext' or 'import numpy' failed!")
     print("Cython extensions will not be compiled, and")
     print("simulations in LFPy may run slower")
     cmdclass = {}
     ext_modules = []
 
-#try and locate the nrnivmodl script of NEURON in PATH so that the
-#NEURON extension file LFPy/sinsyn.mod can be compiled in place and be copied
-#as part of the package_data, allowing unit tests to run
+# try and locate the nrnivmodl script of NEURON in PATH so that the
+# NEURON extension file LFPy/sinsyn.mod can be compiled in place and be copied
+# as part of the package_data, allowing unit tests to run
 from distutils.spawn import find_executable, spawn
 if not any(arg in sys.argv for arg in ['sdist', 'upload']):
     if find_executable('nrnivmodl') is not None:
@@ -43,33 +42,33 @@ if not any(arg in sys.argv for arg in ['sdist', 'upload']):
         spawn([find_executable('nrnivmodl')])
         os.chdir(os.path.join('..', '..'))
     else:
-        print("nrnivmodl script not found in PATH, thus NEURON .mod files could" +
-              "not be compiled, and LFPy.test() functions will fail")
+        print("nrnivmodl script not found in PATH, thus NEURON .mod files" +
+              "could not be compiled, and LFPy.test() functions will fail")
 
 with open('README.md') as file:
     long_description = file.read()
 
 setup(
-    name = "LFPy",
-    version = "2.0.3",
-    maintainer = "Espen Hagen",
-    maintainer_email = 'espen.hagen@fys.uio.no',
-    packages = ['LFPy'],
-    package_data = {'LFPy' : [os.path.join('test', '*.hoc'),
-                              os.path.join('test', '*.py'),
-                              os.path.join('test', 'sinsyn.mod'),
-                              os.path.join('i686', '*'),
-                              os.path.join('i686', '.libs', '*'),
-                              os.path.join('x86_64', '*'),
-                              os.path.join('x86_64', '.libs', '*'),
-                              os.path.join('powerpc', '*'),
-                              os.path.join('powerpc', '.libs', '*'),
-                              ]},
+    name="LFPy",
+    version="2.0.3",
+    maintainer="Espen Hagen",
+    maintainer_email='espen.hagen@fys.uio.no',
+    packages=['LFPy'],
+    package_data={'LFPy': [os.path.join('test', '*.hoc'),
+                           os.path.join('test', '*.py'),
+                           os.path.join('test', 'sinsyn.mod'),
+                           os.path.join('i686', '*'),
+                           os.path.join('i686', '.libs', '*'),
+                           os.path.join('x86_64', '*'),
+                           os.path.join('x86_64', '.libs', '*'),
+                           os.path.join('powerpc', '*'),
+                           os.path.join('powerpc', '.libs', '*'),
+                           ]},
     include_package_data=True,
-    cmdclass = cmdclass,
-    ext_modules = ext_modules,
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
     url='http://LFPy.readthedocs.io',
-    download_url = 'https://github.com/LFPy/LFPy/tarball/v2.0.3',
+    download_url='https://github.com/LFPy/LFPy/tarball/v2.0.3',
     license='LICENSE',
     description='A module for modeling extracellular potentials of multicompartment neuron models built on NEURON',
     long_description=long_description,
@@ -90,14 +89,16 @@ setup(
         'Intended Audience :: Science/Research',
         'Development Status :: 5 - Production/Stable',
         ],
-    install_requires = [
+    install_requires=[
         'numpy>=1.8',
         'scipy>=0.14',
         'Cython>=0.20',
         'h5py>=2.5',
         'mpi4py>=1.2',
         ],
-    extras_require = {'tests': ['pytest']},
-    dependency_links = [],
-    provides = ['LFPy'],
+    extras_require={'tests': ['pytest'],
+                    'docs': ['sphinx <= 1.8.5', 'numpydoc', 'sphinx_rtd_theme']
+                    },
+    dependency_links=[],
+    provides=['LFPy'],
     )
