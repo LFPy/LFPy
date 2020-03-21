669f1f458c1b5a14a0bc5581857a87a776566f75
Espen Hagen <espehage@fys.uio.no>
2020-03-21 18:16:39 +0100

    prep v2.0.5 release

--- LFPy/__init__.py
+++ LFPy/__init__.py
@@ -41,7 +41,7 @@ GNU General Public License for more details.
   * run_simulations - Functions to run NEURON simulations
 """
 
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 
 from .pointprocess import Synapse, PointProcess, StimIntElectrode
 from .recextelectrode import RecExtElectrode, RecMEAElectrode
