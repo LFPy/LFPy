534a4044b0df5b79b6e7606eff44a3434b4dd91d
Espen Hagen <espehage@fys.uio.no>
2020-03-25 15:15:44 +0100

    use v2.0.rc7 version designation

--- LFPy/__init__.py
+++ LFPy/__init__.py
@@ -41,7 +41,7 @@ GNU General Public License for more details.
   * run_simulations - Functions to run NEURON simulations
 """
 
-__version__ = "2.0.7"
+__version__ = "2.0.rc7"
 
 from .pointprocess import Synapse, PointProcess, StimIntElectrode
 from .recextelectrode import RecExtElectrode, RecMEAElectrode
