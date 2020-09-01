74a202df9f4e7c2fe6399a9d3cf13f5d60bd05cc
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2020-09-01 11:45:20 +0200

    fix morphology issue with NEURON>7.7.2

---
+++ LFPy/test/test_morphology_import.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Copyright (C) 2020 Computational Neuroscience Group, NMBU.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+"""
+
+import os
+import unittest
+import numpy as np
+import LFPy
+
+
+class testMorphologyImport(unittest.TestCase):
+    """
+    test that importing a morphology in NEURON results in the correct
+    morphology in LFPy.Cell
+    """
+
+    def test_morphology_import_00(self):
+        cell = LFPy.Cell(morphology=os.path.join(LFPy.__path__[0], 'test',
+                                                 'L23E_p23.hoc'),
+                         nsegs_method=None)
+        data = np.c_[cell.xstart, cell.xmid, cell.xend,
+                     cell.ystart, cell.ymid, cell.yend,
+                     cell.zstart, cell.zmid, cell.zend]
+        expected_data = np.array([
+            [0., 0., 0., 0., 0., 0., -10., 0., 10.],
+            [0., 50., 100., 0., -50., -100., 10., 55., 100.],
+            [0., -50., -100., 0., 50., 100., 10., 55., 100.],
+            [0., 50., 100., 0., 50., 100., -10., -55., -100.],
+            [0., -50., -100., 0., -50., -100., -10., -55., -100.],
+            [0., 0., 0., 0., 0., 0., 10., 167., 324.]])
+
+        np.testing.assert_equal(data, expected_data)
