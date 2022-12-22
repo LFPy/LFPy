81d9cce4599ec1d77b2068121cd1d440a257885a
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-12-22 15:02:52 +0100

    NEURON is pip installable for Python 3.11

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Cython',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering',
